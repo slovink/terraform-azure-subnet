1fb1f08899d0b928842d9f7af282572c37978f1a
krishana <smilekrishana@gmail.com>
2024-02-08 15:53:51 +0530

    update repo

--- _example/name-specific_subnet/output.tf
+++ _example/name-specific_subnet/output.tf
@@ -8,15 +8,6 @@ output "specific_subnet_name" {
   value       = module.name_specific_subnet.specific_subnet_name
 }
 
-output "resource_group_name" {
-  description = "The name of the resource group in which the subnet is created in."
-  value       = module.resource_group.resource_group_name
-}
-
-output "virtual_network_name" {
-  description = "The name of the virtual network in which the subnet is created in."
-  value       = join("", module.vnet.vnet_name)
-}
 
 output "address_prefixes" {
   description = "The address prefixes for the subnet."
@@ -32,4 +23,3 @@ output "route_table_associated_subnets" {
   description = "The collection of Subnets associated with this route table."
   value       = module.name_specific_subnet[*].route_table_associated_subnets[0]
 }
-
